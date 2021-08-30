552a3a3db0a85a22fc331d0079078a6745a633e2
skshad5 <skshad5@gmail.com>
2021-08-31 00:04:11 +0600

    last commit responsive

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ $(window).scroll(function(){
 $('.banner-slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  autoplay: false,
+  autoplay: true,
   dots:true,
   autoplaySpeed: 1000,
   infinite: true,
@@ -57,13 +57,13 @@ $('.banner-slider').slick({
           slidesToScroll: 1
         }
       },
-      // {
-      //   breakpoint: 480,
-      //   settings: {
-      //     slidesToShow: 1,
-      //     slidesToScroll: 1
-      //   }
-      // }
+      {
+        breakpoint: 300,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
      
     ]
   });
@@ -123,6 +123,79 @@ $('.slider-img').slick({
 
 var scrolltotop={setting:{startline:100,scrollto:0,scrollduration:2000,fadeduration:[500,100]},
 controlHTML:'<img src="img/about.png" width="32" height="32"/>',controlattrs:{offsetx:5,offsety:5},anchorkeyword:"#top",state:{isvisible:!1,shouldvisible:!1},scrollup:function(){this.cssfixedsupport||this.$control.css({opacity:0});var t=isNaN(this.setting.scrollto)?this.setting.scrollto:parseInt(this.setting.scrollto);t="string"==typeof t&&1==jQuery("#"+t).length?jQuery("#"+t).offset().top:0,this.$body.animate({scrollTop:t},this.setting.scrollduration)},keepfixed:function(){var t=jQuery(window),o=t.scrollLeft()+t.width()-this.$control.width()-this.controlattrs.offsetx,s=t.scrollTop()+t.height()-this.$control.height()-this.controlattrs.offsety;this.$control.css({left:o+"px",top:s+"px"})},togglecontrol:function(){var t=jQuery(window).scrollTop();this.cssfixedsupport||this.keepfixed(),this.state.shouldvisible=t>=this.setting.startline?!0:!1,this.state.shouldvisible&&!this.state.isvisible?(this.$control.stop().animate({opacity:1},this.setting.fadeduration[0]),this.state.isvisible=!0):0==this.state.shouldvisible&&this.state.isvisible&&(this.$control.stop().animate({opacity:0},this.setting.fadeduration[1]),this.state.isvisible=!1)},init:function(){jQuery(document).ready(function(t){var o=scrolltotop,s=document.all;o.cssfixedsupport=!s||s&&"CSS1Compat"==document.compatMode&&window.XMLHttpRequest,o.$body=t(window.opera?"CSS1Compat"==document.compatMode?"html":"body":"html,body"),o.$control=t('<div id="topcontrol">'+o.controlHTML+"</div>").css({position:o.cssfixedsupport?"fixed":"absolute",bottom:o.controlattrs.offsety,right:o.controlattrs.offsetx,opacity:0,cursor:"pointer"}).attr({title:"Scroll to Top"}).click(function(){return o.scrollup(),!1}).appendTo("body"),document.all&&!window.XMLHttpRequest&&""!=o.$control.text()&&o.$control.css({width:o.$control.width()}),o.togglecontrol(),t('a[href="'+o.anchorkeyword+'"]').click(function(){return o.scrollup(),!1}),t(window).bind("scroll resize",function(t){o.togglecontrol()})})}};scrolltotop.init();
+
+
+// Type JS
+
+// typed js
+$(".typed").typed({
+  strings: ["Web Designer.", "Web Developer.", "Full stack web Developer.", 'Graphics Designer'],
+  // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+  stringsElement: null,
+  // typing speed
+  typeSpeed: 50,
+  // time before typing starts
+  startDelay: 500,
+  // backspacing speed
+  backSpeed: 50,
+  // time before backspacing
+  backDelay: 500,
+  // loop
+  loop: true,
+  // false = infinite
+  loopCount: 100,
+  // show cursor
+  showCursor: false,
+  // character for cursor
+  cursorChar: "|",
+  // attribute to type (null == text)
+  attr: null,
+  // either html or text
+  contentType: 'html',
+  // call when done callback function
+  callback: function() {},
+  // starting callback function before each string
+  preStringTyped: function() {},
+  //callback for every typed string
+  onStringTyped: function() {},
+  // callback for reset
+  resetCallback: function() {}
+});
+
+// typed js service
+$(".service").typed({
+  strings: ["Sk Shad", "Janatul Ferdous Sohana", "Asha Moni", 'Sk Asad'],
+  // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+  stringsElement: null,
+  // typing speed
+  typeSpeed: 50,
+  // time before typing starts
+  startDelay: 500,
+  // backspacing speed
+  backSpeed: 50,
+  // time before backspacing
+  backDelay: 500,
+  // loop
+  loop: true,
+  // false = infinite
+  loopCount: 100,
+  // show cursor
+  showCursor: false,
+  // character for cursor
+  cursorChar: "|",
+  // attribute to type (null == text)
+  attr: null,
+  // either html or text
+  contentType: 'html',
+  // call when done callback function
+  callback: function() {},
+  // starting callback function before each string
+  preStringTyped: function() {},
+  //callback for every typed string
+  onStringTyped: function() {},
+  // callback for reset
+  resetCallback: function() {}
+});
 
 
 });
